b90954c020778f986d1f789c89e8d83167bca27b
Suman Das <suman5848@gmail.com>
2025-08-17 08:49:57 +0000

    Add new file

---
+++ modules/node-group/main.tf
@@ -0,0 +1,44 @@
+resource "aws_eks_node_group" "this" {
+  cluster_name    = var.cluster_name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.config.desired_size
+    max_size     = var.config.max_size
+    min_size     = var.config.min_size
+  }
+
+  disk_size      = var.config.disk_size_gb
+  ami_type       = "AL2023_x86_64_STANDARD" # modern default; switch to BOTTLEROCKET_x86_64 if preferred
+  capacity_type  = "ON_DEMAND"              # change to "SPOT" for spot groups
+  instance_types = var.config.instance_types
+  version        = var.cluster_version
+
+  dynamic "labels" {
+    for_each = length(var.config.labels) > 0 ? [1] : []
+    content {
+      labels = var.config.labels
+    }
+  }
+
+  dynamic "taint" {
+    for_each = var.config.taints
+    content {
+      key    = taint.value.key
+      value  = taint.value.value
+      effect = taint.value.effect
+    }
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = var.tags
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.this.node_group_name
+}
