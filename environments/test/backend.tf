b1c2b8822dd59b4685579721d4ff32c01bc98836
Suman Das <suman5848@gmail.com>
2025-08-17 10:52:58 +0000

    Edit backend.tf

--- environments/test/backend.tf
+++ environments/test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "suman-tf-state-test-s3" #version controlled remote backend
-    key            = "network/terraform.tfstate"
+    key            = "eks/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "suman-tf-state-test-lock"  # DynamoDB table for state locking
     encrypt        = true                    # optional, encrypt state in S3
