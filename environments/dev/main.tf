259a8d26a5b1c0e870647c5ec2a9abecd31042c5
Suman Das <suman5848@gmail.com>
2025-08-17 10:30:32 +0000

    Edit main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,14 +3,36 @@ locals {
 }
 
 module "eks" {
-  source           = "../../modules/eks"
-  cluster_name     = var.cluster_name
-  cluster_version  = var.cluster_version
-  vpc_id           = var.vpc_id
-  subnet_ids       = local.subnets_for_cluster
+  source            = "../../modules/eks"
+  cluster_name      = var.cluster_name
+  cluster_version   = var.cluster_version
+  vpc_id            = var.vpc_id
+  subnet_ids        = local.subnets_for_cluster
   cluster_log_types = var.cluster_log_types
-  tags             = var.tags
+  tags              = var.tags
 }
+
+
+module "node_groups" {
+  source          = "../../modules/node-group"
+  for_each        = var.node_groups
+  cluster_name    = module.eks.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = local.subnets_for_cluster
+  node_group_name = each.key
+  config          = each.value
+  tags            = var.tags
+  node_role_arn   = module.eks.node_role_arn
+}
+
+module "addons" {
+  source          = "../../modules/addons"
+  cluster_name    = module.eks.cluster_name
+  cluster_version = var.cluster_version
+  addon_names     = var.addon_names
+  tags            = var.tags
+}
+
 
 module "node_groups" {
   source          = "../../modules/node-group"
