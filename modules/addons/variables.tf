b951df51b6e589320387a86eba18b98f68862d39
Suman Das <suman5848@gmail.com>
2025-08-17 09:49:59 +0000

    Edit variables.tf

--- modules/addons/variables.tf
+++ modules/addons/variables.tf
@@ -1,4 +1,14 @@
-variable "cluster_name" { type = string }
-variable "cluster_version" { type = string }
-variable "addon_names" { type = list(string) }
-variable "tags" { type = map(string) default = {} }
+variable "cluster_name" {
+     type = string 
+     }
+variable "cluster_version" {
+     type = string
+     }
+variable "addon_names" {
+     type = list(string) 
+     }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
