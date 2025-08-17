81332594a80453eb97aa7f25b59406fa04cd8abc
Suman Das <suman5848@gmail.com>
2025-08-17 13:04:00 +0000

    Edit backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,11 @@
 terraform {
+  required_version = ">= 1.11"
+
   backend "s3" {
-    bucket         = "suman-tf-state-dev-s3" #version controlled remote backend
-    key            = "eks/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "suman-tf-state-dev-lock"  # DynamoDB table for state locking
-    encrypt        = true                    # optional, encrypt state in S3
+    bucket       = "suman-tf-state-dev"
+    key          = "eks/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
