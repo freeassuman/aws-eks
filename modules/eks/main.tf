6d2045239c6105784cedc6d8568cddf1491616ce
Suman Das <suman5848@gmail.com>
2025-08-17 09:03:39 +0000

    eks mod

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,8 @@
-data "aws_caller_identity" "this" {}
 data "aws_partition" "this" {}
+data "tls_certificate" "oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
 
-# EKS control plane role
 resource "aws_iam_role" "eks" {
   name               = "${var.cluster_name}-eks-role"
   assume_role_policy = data.aws_iam_policy_document.eks_assume.json
@@ -21,10 +22,9 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 }
 
 resource "aws_security_group" "cluster" {
-  name        = "${var.cluster_name}-cluster-sg"
-  description = "EKS cluster security group"
-  vpc_id      = var.vpc_id
-  tags        = merge(var.tags, { "kubernetes.io/cluster/${var.cluster_name}" = "owned" })
+  name   = "${var.cluster_name}-cluster-sg"
+  vpc_id = var.vpc_id
+  tags   = merge(var.tags, { "kubernetes.io/cluster/${var.cluster_name}" = "owned" })
 }
 
 resource "aws_eks_cluster" "this" {
@@ -33,24 +33,14 @@ resource "aws_eks_cluster" "this" {
   version  = var.cluster_version
 
   vpc_config {
-    subnet_ids              = var.subnet_ids
-    endpoint_private_access = false
+    subnet_ids         = var.subnet_ids
     endpoint_public_access  = true
-    security_group_ids      = [aws_security_group.cluster.id]
+    endpoint_private_access = false
+    security_group_ids = [aws_security_group.cluster.id]
   }
 
   enabled_cluster_log_types = var.cluster_log_types
-
   tags = var.tags
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-# OIDC provider for IRSA
-data "tls_certificate" "oidc" {
-  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
 resource "aws_iam_openid_connect_provider" "this" {
@@ -60,7 +50,6 @@ resource "aws_iam_openid_connect_provider" "this" {
   tags            = var.tags
 }
 
-# Node IAM role (shared by managed node groups)
 resource "aws_iam_role" "nodes" {
   name               = "${var.cluster_name}-nodes-role"
   assume_role_policy = data.aws_iam_policy_document.nodes_assume.json
@@ -74,7 +63,6 @@ data "aws_iam_policy_document" "nodes_assume" {
   }
 }
 
-# Managed node group baseline policies
 resource "aws_iam_role_policy_attachment" "worker_node" {
   role       = aws_iam_role.nodes.name
   policy_arn = "arn:${data.aws_partition.this.partition}:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -87,8 +75,3 @@ resource "aws_iam_role_policy_attachment" "ecr_ro" {
   role       = aws_iam_role.nodes.name
   policy_arn = "arn:${data.aws_partition.this.partition}:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-
-output "cluster_name"         { value = aws_eks_cluster.this.name }
-output "cluster_endpoint"     { value = aws_eks_cluster.this.endpoint }
-output "oidc_provider_arn"    { value = aws_iam_openid_connect_provider.this.arn }
-output "node_role_arn"        { value = aws_iam_role.nodes.arn }
